6745fc4429201052dd3b4ef47f0336d2017a64d1
usrsem <user7em@gmail.com>
2020-12-30 18:58:02 +0300

    Initial Commit

---
+++ LabsForDesya/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LabsForDesya
+//
+//  Created by usrsem . on 12/30/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
